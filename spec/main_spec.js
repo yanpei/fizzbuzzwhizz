cb8a452d136ba6874f0cca7026020e78ce862229
yanpei <1159319529@qq.com>
2016-02-25 18:59:36 +0800

    test_contain_a&&b&c

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -73,6 +73,12 @@ describe("测试描述", function(){
        expect(expect_string).to.equal(result);
     });
 
-    
+    it("测试用例13",function(){
+       var result = main(3,5,7,35);
+       var expect_string = 'Fizz';
+       expect(expect_string).to.equal(result);
+    });
+
+
 
 });
